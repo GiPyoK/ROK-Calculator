33d8f6e9d39fed7d4f76f73ed4a9a98533349288
Gi Pyo Kim <gipyok@gmail.com>
2020-08-02 13:37:51 -0700

    Initial Commit

---
+++ ROK-Helper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ROK-Helper
+//
+//  Created by Gi Pyo Kim on 8/2/20.
+//  Copyright © 2020 gipgip. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
