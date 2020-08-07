c76e9cfbdc8396ad8163ecb6c223e0115b2269ad
Gi Pyo Kim <gipyok@gmail.com>
2020-08-07 00:01:28 -0700

    Utilized the SpeedupListViewModel and cleaned the view

--- ROK-Helper/Views/ContentView.swift
+++ ROK-Helper/Views/ContentView.swift
@@ -8,14 +8,25 @@
 
 import SwiftUI
 
+enum sUpName: String {
+    case train
+    case build
+    case research
+    case universal
+}
+
 struct ContentView: View {
     
     // https://stackoverflow.com/questions/56491881/move-textfield-up-when-the-keyboard-has-appeared-in-swiftui
     // Good keyboard animation
     
+    @ObservedObject var speedupListVM: SpeedupListViewModel
+    
+    init() {
+        self.speedupListVM = SpeedupListViewModel()
+    }
+    
     @State var universalSpeedup: String = ""
-
-    @State var yOffset : CGFloat = 0
     
     var body: some View {
         VStack{
@@ -28,30 +39,6 @@ struct ContentView: View {
                 HStack {
                     VStack {
                         Text("1 min")
-                        TextField("#", text:$universalSpeedup)
-                            .keyboardType(.numberPad)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-
-                    }.padding()
-                        .frame(width: 100, height: 100)
-                    VStack {
-                        Text("5 min")
-                        TextField("#", text:$universalSpeedup)
-                            .keyboardType(.numberPad)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-
-                    }.padding()
-                        .frame(width: 100, height: 100)
-                    VStack {
-                        Text("10 min")
-                        TextField("#", text:$universalSpeedup)
-                            .keyboardType(.numberPad)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-
-                    }.padding()
-                        .frame(width: 100, height: 100)
-                    VStack {
-                        Text("15 min")
                         TextField("#", text:$universalSpeedup)
                             .keyboardType(.numberPad)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
@@ -78,43 +65,10 @@ struct ContentView: View {
 
                     }.padding()
                         .frame(width: 100, height: 100)
-                    VStack {
-                        Text("5 min")
-                        TextField("#", text:$universalSpeedup)
-                            .keyboardType(.numberPad)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-
-                    }.padding()
-                        .frame(width: 100, height: 100)
-                    VStack {
-                        Text("10 min")
-                        TextField("#", text:$universalSpeedup)
-                            .keyboardType(.numberPad)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-
-                    }.padding()
-                        .frame(width: 100, height: 100)
-                    VStack {
-                        Text("15 min")
-                        TextField("#", text:$universalSpeedup)
-                            .keyboardType(.numberPad)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-
-                    }.padding()
-                        .frame(width: 100, height: 100)
                 }
             }
         }.padding()
             
-        }.onAppear {
-            NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { key in
-                let value = key.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
-                self.yOffset = value.height
-            }
-            
-            NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { key in
-                self.yOffset = 0
-            }
         }
         }
 }
