1586e6d57bac921e0a321ddd57098f8b4ef9f275
Gi Pyo Kim <gipyok@gmail.com>
2020-08-10 23:22:14 -0700

    Implemented calculate sum function and added sum variable for each speedups

--- ROK-Helper/ViewModels/SpeedUpViewModel.swift
+++ ROK-Helper/ViewModels/SpeedUpViewModel.swift
@@ -19,22 +19,22 @@ enum SpeedupTypes: String {
     case universal
 }
 
-enum TimeTypes: String, CaseIterable {
-    case min1
-    case min5
-    case min10
-    case min15
-    case min30
-    case min60
+enum TimeTypes: Int, CaseIterable {
+    case min1 = 1
+    case min5 = 5
+    case min10 = 10
+    case min15 = 15
+    case min30 = 30
+    case min60 = 60
     
-    case hour3
-    case hour8
-    case hour15
-    case hour24
+    case hour3 = 180
+    case hour8 = 480
+    case hour15 = 900
+    case hour24 = 1440
     
-    case day3
-    case day7
-    case day30
+    case day3 = 4320
+    case day7 = 10080
+    case day30 = 43200
 }
 
 class SpeedupListViewModel: ObservableObject {
@@ -106,6 +106,13 @@ class SpeedupListViewModel: ObservableObject {
                              ("day7", ""),
                              ("day30", "") ]
     
+    // In minutes
+    @Published var universalSum = 0
+    @Published var trainlSum = 0
+    @Published var researchSum = 0
+    @Published var buildSum = 0
+    @Published var healSum = 0
+    
     private var speedups = [SpeedUpViewModel]()
     
     init() {
@@ -130,6 +137,46 @@ class SpeedupListViewModel: ObservableObject {
     
     func fetchAllSpeedups() {
         self.speedups = CoreDataManager.shared.getAllSpeedups().map(SpeedUpViewModel.init)
+    }
+    
+    func calculateUniversalSum() {
+        self.universalSum = 0
+        for i in 0..<13 {
+            let minute = Int(self.uSpeedups[i].1) ?? 0
+            self.universalSum += (minute * TimeTypes.allCases[i].rawValue)
+        }
+    }
+    
+    func calculateTrainSum() {
+        self.trainlSum = 0
+        for i in 0..<13 {
+            let minute = Int(self.tSpeedups[i].1) ?? 0
+            self.trainlSum += (minute * TimeTypes.allCases[i].rawValue)
+        }
+    }
+    
+    func calculateResearchSum() {
+        self.researchSum = 0
+        for i in 0..<13 {
+            let minute = Int(self.rSpeedups[i].1) ?? 0
+            self.researchSum += (minute * TimeTypes.allCases[i].rawValue)
+        }
+    }
+    
+    func calculateBuildSum() {
+        self.buildSum = 0
+        for i in 0..<13 {
+            let minute = Int(self.bSpeedups[i].1) ?? 0
+            self.buildSum += (minute * TimeTypes.allCases[i].rawValue)
+        }
+    }
+    
+    func calculateHealSum() {
+        self.healSum = 0
+        for i in 0..<13 {
+            let minute = Int(self.hSpeedups[i].1) ?? 0
+            self.healSum += (minute * TimeTypes.allCases[i].rawValue)
+        }
     }
     
     private func getRecentSpeedups() {
